931d9d820cde7a01683c43fae4d7d13de8dfa4d4
schulke-214 <schulkemaximilian@gmail.com>
2019-12-27 00:58:10 +0100

    refactor(domain/models): rename categories to category

--- pkg/domain/models/category.go
+++ pkg/domain/models/category.go
@@ -1,11 +1,7 @@
 package models
 
-import (
-	"github.com/jinzhu/gorm"
-)
-
 // Category is the core datatype this app is built around.
 type Category struct {
-	gorm.Model
-	Name string
+	BaseModel
+	Name string `json:"name" binding:"required"`
 }
