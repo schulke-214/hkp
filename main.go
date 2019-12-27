c122c4c6a7e10fb2a980673cac080aab4649e219
schulke-214 <schulkemaximilian@gmail.com>
2019-12-27 03:52:24 +0100

    refactor: inline the route registering

--- main.go
+++ main.go
@@ -4,13 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 
-	"github.com/schulke-214/hkp/api"
+	"github.com/schulke-214/hkp/api/tasks"
 	"github.com/schulke-214/hkp/pkg/db"
 )
 
 func main() {
 	router := gin.Default()
-	api.RegisterCategories(router)
+
+	taskRouter := router.Group("/tasks")
+
+	taskRouter.POST("/", tasks.Create)
+	taskRouter.GET("/", tasks.QueryAll)
+	taskRouter.GET("/:id", tasks.QueryByID)
+	taskRouter.PATCH("/:id", tasks.UpdateByID)
+	taskRouter.DELETE("/:id", tasks.DeleteByID)
+
+	//
+	entriesRouter := taskRouter.Group("/:id/entries")
+
+	entriesRouter.GET("/", tasks.QueryAllEntries)
+	entriesRouter.POST("/", tasks.CreateEntry)
+	entriesRouter.DELETE("/:entry_id", tasks.DeleteEntry)
+
 	router.Run(":8080")
 
 	defer db.Connection.Close()
