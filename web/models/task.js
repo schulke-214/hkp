aa60cb7c4e3465ac4a575a5e9dd20cead5486184
schulke-214 <schulkemaximilian@gmail.com>
2020-01-02 23:46:41 +0100

    fix: delete unused models

--- web/models/task.js
+++
@@ -1,9 +0,0 @@
-export class Task {
-	constructor(task) {
-		this.id = task.id;
-		this.created_at = task.created_at;
-		this.updated_at = task.updated_at;
-		this.deleted_at = task.deleted_at;
-		this.name = task.name;
-	}
-}
