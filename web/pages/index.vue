abbaf1db1c8ceb1366d4eed159f62dea67d030a5
schulke-214 <schulkemaximilian@gmail.com>
2020-01-02 23:42:30 +0100

    feat(ui): add a main page which composes sub components

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,47 +1,34 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub</a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">Every</b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">Vue.js</a> and <a href="http://bulma.io/">Bulma</a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-
-    </div>
-  </section>
+	<div>
+		<section class="section">
+			<div class="columns" style="flex-wrap: wrap;">
+				<task v-for="task in tasks" :key="task.id" :task-id="task.id" @delete="remove(task.id)" />
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
-import Card from '~/components/Card'
+import { mapState } from 'vuex';
+
+import Task from '~/components/core/task';
+import TaskForm from '~/components/core/task-form';
 
 export default {
-  name: 'HomePage',
-
-  components: {
-    Card
-  }
-}
+	name: 'index',
+	components: {
+		Task,
+		TaskForm
+	},
+	methods: {
+		async remove(id) {
+			await this.$store.dispatch('tasks/delete', { id }, { root: true });
+		}
+	},
+	computed: {
+		tasks() {
+			return this.$store.state.tasks.data;
+		}
+	}
+};
 </script>
