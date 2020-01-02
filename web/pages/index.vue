7be0cfb68ecc8726ffb5cc4a8899975c02880e79
schulke-214 <schulkemaximilian@gmail.com>
2020-01-03 00:20:18 +0100

    feat: add a message if there no tasks

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -3,6 +3,7 @@
 		<section class="section">
 			<div class="columns" style="flex-wrap: wrap;">
 				<task v-for="task in tasks" :key="task.id" :task-id="task.id" @delete="remove(task.id)" />
+				<b-message v-if="!tasks.length">Sieht aus als wären da noch keine aufgaben...</b-message>
 			</div>
 		</section>
 	</div>
