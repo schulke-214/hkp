be13162506d9a778c6f9af8bd0b18c9bff065028
schulke-214 <schulkemaximilian@gmail.com>
2020-01-03 00:20:35 +0100

    refactor: use an icon instead of the buefy logo

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -2,11 +2,9 @@
 	<div>
 		<b-navbar>
 			<template #brand>
-				<b-navbar-item tag="router-link" :to="{ path: '/' }">
-					<img
-						src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-						alt="HKP"
-					/>
+				<b-navbar-item tag="nuxt-link" to="/">
+					HKP
+					<!-- poll-h -->
 				</b-navbar-item>
 				<b-navbar-item tag="div">{{ greeting }}</b-navbar-item>
 			</template>
