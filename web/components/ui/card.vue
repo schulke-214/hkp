a81e4e5e971607dd3350a0b60e88e0a5c27a477a
schulke-214 <schulkemaximilian@gmail.com>
2020-01-02 23:41:22 +0100

    feat(ui): add a card and a loading component

---
+++ web/components/ui/card.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="column" style="min-width: 200px;">
+		<div class="card">
+			<header v-if="header" class="card-header">
+				<p class="card-header-title">{{ title }}</p>
+				<slot name="header" />
+			</header>
+			<div class="card-content">
+				<slot />
+			</div>
+			<footer v-if="footer" class="card-footer">
+				<div class="card-footer-item">
+					<slot name="footer" />
+				</div>
+			</footer>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: true
+		},
+		header: {
+			type: Boolean,
+			default: true
+		},
+		footer: {
+			type: Boolean,
+			default: true
+		}
+	}
+};
+</script>
---
+++ web/components/ui/loading.vue
@@ -0,0 +1,19 @@
+<template>
+	<b-loading :is-full-page="true" :active.sync="loading" v-show="false"></b-loading>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex';
+
+export default {
+	computed: mapGetters({
+		loading: 'loading/status'
+	}),
+	methods: mapMutations({
+		start: 'loading/start',
+		finish: 'loading/stop'
+	})
+};
+</script>
+
+<style></style>
